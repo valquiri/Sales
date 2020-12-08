53f4e9534a9c73ccd8d41d1c45b3b92e02f4477a
Valquiria Santos <valquiria.felix.vf@gmail.com>
2020-12-07 22:57:34 -0300

    Projeto created

---
+++ SlaesWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SlaesWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
